f0b458f59f7eace4f4b9b6946420019de8d56d9f
Smovg-by <Smovg-by@users.noreply.github.com>
2021-11-17 20:18:23 +0300

    App 1 complete

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -2,13 +2,18 @@ import ChartBar from './ChartBar'
 import './Chart.css'
 
 const Chart = ({ dataPoints }) => {
+
+  const datapointsValues = dataPoints.map(dataPoint => dataPoint.value)
+
+  const totalMaximum = Math.max(...datapointsValues)
+
   return (
     <div className={'chart'}>
       {dataPoints.map(dataPoint => (
         <ChartBar
           key={dataPoint.label}
           value={dataPoint.value}
-          maxValue={null}
+          maxValue={totalMaximum}
           label={dataPoint.label}
         />
       ))}
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,26 +1,29 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import Card from '../UI/Card'
 import './Expenses.css'
-import ExpensesFilter from "./ExpensesFilter";
-import ExpensesList from "./ExpensesList";
+import ExpensesFilter from './ExpensesFilter'
+import ExpensesList from './ExpensesList'
+import ExpensesChart from './ExpensesChart'
 
-
-function Expenses({items}) {
-
-  const [filteredYear, setFilteredYear] = useState('2020');
+function Expenses ({ items }) {
+  const [filteredYear, setFilteredYear] = useState('2020')
 
   const filterChangeHandler = selectedYear => {
     setFilteredYear(selectedYear)
-  };
+  }
 
-  const filteredExpenses = items.filter(expense => expense.date.getFullYear().toString() === filteredYear);
+  const filteredExpenses = items.filter(
+    expense => expense.date.getFullYear().toString() === filteredYear
+  )
 
   return (
     <div>
       <Card className={'expenses'}>
         <ExpensesFilter
           selected={filteredYear}
-          onChangeFilter={filterChangeHandler}/>
+          onChangeFilter={filterChangeHandler}
+        />
+        <ExpensesChart expenses={filteredExpenses} />
         <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
