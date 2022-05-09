409201ddd42524db4365918f970ff59875dbc116
Smovg-by <Smovg-by@users.noreply.github.com>
2022-05-09 11:05:18 +0300

    lesson 395. Testing react app

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
-import React, { useState } from 'react';
-
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import logo from './logo.svg';
+import './App.css';
+import Greeting from './components/Greeting';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  const loginHandler = (email, password) => {
-    // We should of course check email and password
-    // But it's just a dummy/ demo anyways
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    setIsLoggedIn(false);
-  };
-
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
-    </React.Fragment>
+    <div className="App">
+      <Greeting />
+    </div>
   );
 }
 
